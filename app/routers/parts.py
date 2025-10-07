dee6cc380733a136033170a1a9bc1ca866b61cf7
botirjon13 <tomama-uz@mail.ru>
2025-10-07 08:57:20 +0500

    Update parts.py

--- app/routers/parts.py
+++ app/routers/parts.py
@@ -8,24 +8,24 @@ from app.routers.auth import get_current_user
 router = APIRouter()
 
 @router.post('/', response_model=schemas.PartOut)
-def create_part(p: schemas.PartBase, db: Session = Depends(next(get_db)), user=Depends(get_current_user)):
+def create_part(p: schemas.PartBase, db: Session = Depends(get_db), user=Depends(get_current_user)):
     part = models.Part(**p.dict())
     db.add(part); db.commit(); db.refresh(part)
     return part
 
 @router.get('/', response_model=List[schemas.PartOut])
-def list_parts(skip:int=0, limit:int=100, db: Session = Depends(next(get_db)), user=Depends(get_current_user)):
+def list_parts(skip:int=0, limit:int=100, db: Session = Depends(get_db), user=Depends(get_current_user)):
     return db.query(models.Part).offset(skip).limit(limit).all()
 
 @router.get('/{id}', response_model=schemas.PartOut)
-def get_part(id:int, db: Session = Depends(next(get_db)), user=Depends(get_current_user)):
+def get_part(id:int, db: Session = Depends(get_db), user=Depends(get_current_user)):
     part = db.query(models.Part).get(id)
     if not part:
         raise HTTPException(status_code=404, detail='Not found')
     return part
 
 @router.put('/{id}', response_model=schemas.PartOut)
-def update_part(id:int, p: schemas.PartBase, db: Session = Depends(next(get_db)), user=Depends(get_current_user)):
+def update_part(id:int, p: schemas.PartBase, db: Session = Depends(get_db), user=Depends(get_current_user)):
     part = db.query(models.Part).get(id)
     if not part:
         raise HTTPException(status_code=404, detail='Not found')
@@ -35,7 +35,7 @@ def update_part(id:int, p: schemas.PartBase, db: Session = Depends(next(get_db))
     return part
 
 @router.delete('/{id}')
-def delete_part(id:int, db: Session = Depends(next(get_db)), user=Depends(get_current_user)):
+def delete_part(id:int, db: Session = Depends(get_db), user=Depends(get_current_user)):
     part = db.query(models.Part).get(id)
     if not part:
         raise HTTPException(status_code=404, detail='Not found')
