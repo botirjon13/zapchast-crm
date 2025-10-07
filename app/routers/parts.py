711cf784f968c59c34a08e86970b56ed86f95d4b
botirjon13 <tomama-uz@mail.ru>
2025-10-07 16:15:22 +0500

    Update parts.py

--- app/routers/parts.py
+++ app/routers/parts.py
@@ -3,7 +3,6 @@ from typing import List
 from sqlalchemy.orm import Session
 from app import models, schemas
 from app.database import get_db
-from app.routers.auth import get_current_user
 
 router = APIRouter()
 
