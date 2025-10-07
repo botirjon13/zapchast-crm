3d7c305f22c80ce86c06c4f22a9491ffdc19973c
botirjon13 <tomama-uz@mail.ru>
2025-10-07 16:28:51 +0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,16 +1,13 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///./test.db')
+SQLALCHEMY_DATABASE_URL = "sqlite:///./store.db"
 
-# For PostgreSQL ensure DATABASE_URL like: postgresql://user:pass@host:port/dbname
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {})
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Dependency
 def get_db():
     db = SessionLocal()
     try:
