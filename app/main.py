3cdf6928125e837757bbc08a0c7ea45541944f0f
botirjon13 <tomama-uz@mail.ru>
2025-10-07 18:55:12 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
-from app.database import engine, Base
-from app.routers import auth, customers, parts, orders
+from .database import engine, Base
+from . import models
+from .routers import auth, customers, parts, orders, stats
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Store Management")
+app = FastAPI(title='Zapchat CRM')
 
-app.include_router(auth.router)
+app.include_router(auth.router, prefix='/auth')
 app.include_router(customers.router)
 app.include_router(parts.router)
 app.include_router(orders.router)
+app.include_router(stats.router)
+
+from fastapi.staticfiles import StaticFiles
+app.mount('/static', StaticFiles(directory='app/static'), name='static')
