e04dba530a379c420d660ee1390d6c144dc13b0e
botirjon13 <tomama-uz@mail.ru>
2025-10-07 08:54:06 +0500

    Update customers.py

--- app/routers/customers.py
+++ app/routers/customers.py
@@ -8,24 +8,24 @@ from app.routers.auth import get_current_user
 router = APIRouter()
 
 @router.post('/', response_model=schemas.CustomerOut)
-def create_customer(c: schemas.CustomerBase, db: Session = Depends(next(get_db)), user=Depends(get_current_user)):
+def create_customer(c: schemas.CustomerBase, db: Session = Depends(get_db), user=Depends(get_current_user)):
     cust = models.Customer(**c.dict())
     db.add(cust); db.commit(); db.refresh(cust)
     return cust
 
 @router.get('/', response_model=List[schemas.CustomerOut])
-def list_customers(skip: int=0, limit: int=100, db: Session = Depends(next(get_db)), user=Depends(get_current_user)):
+def list_customers(skip: int=0, limit: int=100, db: Session = Depends(get_db), user=Depends(get_current_user)):
     return db.query(models.Customer).offset(skip).limit(limit).all()
 
 @router.get('/{id}', response_model=schemas.CustomerOut)
-def get_customer(id: int, db: Session = Depends(next(get_db)), user=Depends(get_current_user)):
+def get_customer(id: int, db: Session = Depends(get_db), user=Depends(get_current_user)):
     cust = db.query(models.Customer).get(id)
     if not cust:
         raise HTTPException(status_code=404, detail='Not found')
     return cust
 
 @router.put('/{id}', response_model=schemas.CustomerOut)
-def update_customer(id:int, c: schemas.CustomerBase, db: Session = Depends(next(get_db)), user=Depends(get_current_user)):
+def update_customer(id:int, c: schemas.CustomerBase, db: Session = Depends(get_db), user=Depends(get_current_user)):
     cust = db.query(models.Customer).get(id)
     if not cust:
         raise HTTPException(status_code=404, detail='Not found')
@@ -35,7 +35,7 @@ def update_customer(id:int, c: schemas.CustomerBase, db: Session = Depends(next(
     return cust
 
 @router.delete('/{id}')
-def delete_customer(id:int, db: Session = Depends(next(get_db)), user=Depends(get_current_user)):
+def delete_customer(id:int, db: Session = Depends(get_db), user=Depends(get_current_user)):
     cust = db.query(models.Customer).get(id)
     if not cust:
         raise HTTPException(status_code=404, detail='Not found')
