8c859080cbb2218c7bb3e943291a5465cfb3abef
botirjon13 <tomama-uz@mail.ru>
2025-10-07 15:22:46 +0500

    Update core.py

--- app/core.py
+++ app/core.py
@@ -1,7 +1,10 @@
 import os
 SECRET_KEY = os.getenv('SECRET_KEY', 'supersecret')
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 60*24))
-# Default single admin credentials (you can change these or set env vars)
+
+
+# Default single admin (used when single-admin mode)
+import os
 DEFAULT_ADMIN_USERNAME = os.getenv('DEFAULT_ADMIN_USERNAME', 'admin')
 DEFAULT_ADMIN_PASSWORD = os.getenv('DEFAULT_ADMIN_PASSWORD', '1234')
 DEFAULT_ADMIN_FULLNAME = os.getenv('DEFAULT_ADMIN_FULLNAME', 'Administrator')
