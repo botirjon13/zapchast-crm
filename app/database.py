b47e31f5a7042675a8b31497a63997ea88308162
botirjon13 <tomama-uz@mail.ru>
2025-10-07 18:54:58 +0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,20 +1,11 @@
-# app/database.py
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Agar SQLite ishlatayotgan bo‘lsangiz:
-SQLALCHEMY_DATABASE_URL = "sqlite:///./store.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./zapchat.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# MUHIM: Base shu yerda e’lon qilinadi
 Base = declarative_base()
 
-# DB session olish uchun
 def get_db():
     db = SessionLocal()
     try:
